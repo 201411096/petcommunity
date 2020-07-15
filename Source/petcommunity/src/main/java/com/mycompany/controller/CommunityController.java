e8cc5ba13cf6bc3ea868ae68f7267d57aefbd2d6
young <syckor@naver.com>
2020-07-15 19:13:52 +0900

    커뮤니티 게시판 뼈대
    뼈대 만듬

---
+++ Source/petcommunity/src/main/java/com/mycompany/controller/CommunityController.java
@@ -0,0 +1,20 @@
+package com.mycompany.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.service.CommunityService;
+
+@Controller
+public class CommunityController {
+	
+	@Autowired
+	private CommunityService communityService;
+	
+	@RequestMapping("/getCommunityBoardList.do")
+	public String getCommunityBoardList() {
+		return "getCommunityBoardList";
+	}
+	
+}

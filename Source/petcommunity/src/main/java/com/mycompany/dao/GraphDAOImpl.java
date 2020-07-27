6e67910178cbbf885d914003ef47c2a028f735f7
김용선 <korea5781@gmail.com>
2020-07-27 16:58:18 +0900

    200727_02 실종동물 요일별, 시간별 통계

---
+++ Source/petcommunity/src/main/java/com/mycompany/dao/GraphDAOImpl.java
@@ -0,0 +1,21 @@
+package com.mycompany.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("graphDAO")
+public class GraphDAOImpl implements GraphDAO{
+	@Autowired
+	private SqlSessionTemplate mybatis;
+
+	@Override
+	public List<Map> getLostGraph(Map map) {
+		return mybatis.selectList("graphDAO.getLostGraph", map);
+	}
+	
+	
+}

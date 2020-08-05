6ef19a587f2245e36d3189cf9449fb717b1a7c69
park seyeong <se_ooo@naver.com>
2020-08-05 12:04:10 +0900

    sh
    sh

--- Source/petcommunity/src/main/java/com/mycompany/dao/AdminDAO.java
+++ Source/petcommunity/src/main/java/com/mycompany/dao/AdminDAO.java
@@ -1,9 +1,11 @@
 package com.mycompany.dao;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.mycompany.domain.BuylistviewVO;
+import com.mycompany.domain.ManagerVO;
 import com.mycompany.domain.MemberVO;
 
 public interface AdminDAO {
@@ -13,5 +15,9 @@ public interface AdminDAO {
 	public List<MemberVO> getgetMemberList(MemberVO membervo);
 	public List<MemberVO> getMemberSelect(Map map);
 	public int deleteInfo(String memberId);
-
+	public int managerInsert(HashMap map);
+	public List<ManagerVO> managerSelect();
+	public int managerupdate(HashMap map);
+	public List<ManagerVO> checkId(String id);
+	public void managerDelete(String id);
 }

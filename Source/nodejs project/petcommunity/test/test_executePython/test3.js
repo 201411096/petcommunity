5ad51bdecd28022fd10753a505ab9720f9c120bf
김용선 <korea5781@gmail.com>
2020-08-07 13:18:50 +0900

    200807_05

--- Source/nodejs project/petcommunity/test/test_executePython/test3.js
+++ Source/nodejs project/petcommunity/test/test_executePython/test3.js
@@ -18,14 +18,14 @@ var options = {
     pythonPath: 'C:/ProgramData/Anaconda3/python.exe', //python의 설치경로를 입력하는 부분
     pythonOptions: ['-u'],
     scriptPath: '',
-    args: [1]
+    // args:[1],
+    args: [0, '20200801', '20200831', '100']
 }
 
 PythonShell.run(directoryPath, options, function (err, results) {
-    console.log(directoryPath);
+    console.log("check directoryPath in PythonShell.run() ..." + directoryPath);
     if (err) throw err;
   
-  
+    
     console.log('results: %j', results);
-  
   });

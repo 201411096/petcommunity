73d3967d83a94c0500a766b6f035fd7e2ddb0659
김용선 <korea5781@gmail.com>
2020-08-16 16:04:24 +0900

    200816_06

--- Source/nodejs project/petcommunity/app.js
+++ Source/nodejs project/petcommunity/app.js
@@ -130,7 +130,7 @@ function messageHandling(memberId, msg, socket){ // memberId가 보내는 사람
     if(firstArgument[0]=="/"){                                                                                                                      // 명령어 처리 부분 시작 ---
       if(firstArgument=="/help" || firstArgument=="/도움말" || firstArgument=="/h"){   // 도움말 처리 시작 ---
         io.to(socket.id).emit('chat message', makeMessageObject('system', '========================================'));
-        io.to(socket.id).emit('chat message', makeMessageObject('system', '귓속말 사용방법 ==> [/귓속말][/whisper][/w][/ㅈ] 상대방닉네임 내용'));
+        io.to(socket.id).emit('chat message', makeMessageObject('system', '귓속말 사용방법 ==> [/귓속말][/whisper][/w][/ㅈ] 상대닉네임 내용'));
         io.to(socket.id).emit('chat message', makeMessageObject('system', '채팅창 지우기 ==> /clear'));
         io.to(socket.id).emit('chat message', makeMessageObject('system', '채팅창 닫기 ==> /exit'));
         io.to(socket.id).emit('chat message', makeMessageObject('system', '========================================'));

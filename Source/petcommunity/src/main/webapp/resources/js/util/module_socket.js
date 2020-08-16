50f60fa50c79401f9375eff311b992c425cc8aa9
김용선 <korea5781@gmail.com>
2020-08-16 22:01:33 +0900

    200816_10

--- Source/petcommunity/src/main/webapp/resources/js/util/module_socket.js
+++ Source/petcommunity/src/main/webapp/resources/js/util/module_socket.js
@@ -1,18 +1,16 @@
-//var socket = io("https://192.168.0.18:3000");   // 내부ip  
-var socket = io("https://115.91.88.227:60005"); // 대회의실용
+//var socket = io("https://192.168.0.18:3000");   // 내부ip
 //var socket = io("https://121.171.119.57:3000"); // 집
+var socket = io("https://115.91.88.227:60005"); // 대회의실용
 
-setNickname();
-receiveMessage();
-var messagePageFlag =0;
-
-//소켓 닉네임 로그인 id로 설정
+setNickname();			// 소켓의 닉네임을 설정하는 함수
+receiveMessage();		// 쪽지를 받는 함수
+var messagePageFlag =0; 
+//소켓 닉네임을 로그인 id로 설정
 function setNickname(){
 	var memberId=$("#hiddenId").val();
 	   socket.emit('setNickname', memberId);
 	   console.log(memberId);
 }
-
 function toastMessage(messageContent, messageFrom, messageTo){
 	toastr["info"](messageFrom+'<a href="/petcommunity/getChatPartner.do?messageFrom='+messageFrom+'">바로가기</a>'+"<br /><br />"+messageContent);
 	toastr.options = {
@@ -36,22 +34,13 @@ function toastMessage(messageContent, messageFrom, messageTo){
 			}
 }
 function receiveMessage(){
-	console.log('receiveMessage 함수 호출 확인')
 	socket.on('sendMessageData', function(data){
-	 console.log('receiveMessage evt');
-	 console.log(data.messageContent);
-	 console.log(data.messageFrom);
-	 console.log(data.messageTo);
 	 if(messagePageFlag==1){
 		 getChat(startPage, endPage);
 	 }
 	 toastMessage(data.messageContent, data.messageFrom, data.messageTo);
  });
 	socket.on('sendDelData', function(data){
-		console.log('receiveMessageDelete evt');
-		 console.log(data.msgId);
-		 console.log("보낸사람"+data.messageFrom);
-		 console.log(data.messageTo);
 		 if(messagePageFlag==1){
 			 getChat(startPage, endPage);
 		 }
